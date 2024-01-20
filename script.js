bb86f744c184d14dfa2d29595ca2205695e4f465
Himumi <erliansyaha@gmail.com>
2024-01-20 23:01:27 +0900

    Create function for display number and operator

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ const getOperator = document.querySelectorAll(".btn-operator");
 getOperator.forEach((btn) => {
     btn.addEventListener("click", () => {
         operator = btn.value;
+
+        display();
     });
 });
 
@@ -17,6 +19,8 @@ getNumber.forEach((btn) => {
         }else {
             firstNumber.push(btn.value);
         }
+
+        display();
     });
 });
 
@@ -31,6 +35,7 @@ removeByOne.addEventListener("click", () => {
     }else{
         lastNumber.pop();
     }
+    display();
 });
 
 const removeAll = document.querySelector(".allclear");
@@ -41,4 +46,45 @@ removeAll.addEventListener("click", () => {
     // firstNumber.splice(0,firstNumber.length);
     // lastNumber.splice(0,lastNumber.length);
     operator = undefined;
+
+    display();
 });
+
+const createDiv = document.createElement("div");
+const displayDiv = document.querySelector(".display");
+displayDiv.append(createDiv);
+createDiv.textContent = "0";
+
+function display() {
+    let checkedOperator = checkOperation(operator);
+    if (operator !== undefined) {
+        createDiv.textContent = firstNumber.join("") + " " + checkedOperator + " " + lastNumber.join("");
+    } else{
+        if (firstNumber.length === 0){
+            createDiv.textContent = "0";
+        }else{
+            createDiv.textContent = firstNumber.join("");
+        }
+    }
+}
+
+function checkOperation(value){
+    switch (value) {
+        case "addition":
+            return "+";
+        break;
+        case "subtraction":
+            return "-";
+        break;
+        case "multiplication":
+            return "*";
+        break;
+        case "division":
+            return "/";
+        break;
+        case "exponent":
+            return "^";
+        break;
+    }
+}
+
